bb1b3ea9ffc2a9e924711a41e1e41490d155a24e
Ye (Shawn) Wang <wang77289271@gmail.com>
2024-03-04 17:50:27 -0700

    edit header desktop

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import ButtonGradient from './assets/svg/ButtonGradient'
 import Button from './components/Button'
+import Header from './components/Header'
 
 function App() {
   return (
     <>
-      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
-
       <div className='pt-[4.75em] lg:pt-[5.25em] overflow-hidden'>
-        <Button className='mt-10' href='#login'>
-          hello
-        </Button>
+        <Header />
       </div>
 
       <ButtonGradient />
