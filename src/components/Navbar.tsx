ed9b64131658e3c64388ba18440440bc7b4c31f5
Ye (Shawn) Wang <wang77289271@gmail.com>
2024-05-26 19:43:38 -0600

    small edit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -30,7 +30,7 @@ const Navbar = ({ navigation }: { navigation: Navigation[] }) => {
   return (
     <div
       className={`fixed top-0 left-0 w-full z-50 border-b border-n-6
-       lg:bg-n-8/90 lg:backdrop-blur-sm ${
+       lg:bg-n-8/70 lg:backdrop-blur-sm ${
          openNavigation ? 'bg-n-8' : 'bg-n-8/90 backdrop-blur-sm'
        }`}
     >
@@ -47,7 +47,7 @@ const Navbar = ({ navigation }: { navigation: Navigation[] }) => {
         <nav
           className={`${
             openNavigation ? 'flex' : 'hidden'
-          } fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent'`}
+          } fixed top-[5rem] left-0 right-0 bottom-0  lg:static lg:flex lg:mx-auto lg:bg-transparent'`}
         >
           <div className='relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row'>
             {navigation.map((item) => (
